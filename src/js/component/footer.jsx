204aa72abc1cac86558d25fc2ac2963c029940d7
Logan Havens <153969446+Logan-Havens@users.noreply.github.com>
2024-03-08 23:56:38 +0000

    made the footer

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './footerStyles.css'
+
+
+//create your first component
+const Footer = () => {
+	return (
+		<div className="Footer">
+
+        </div>
+	
+)};
+
+export default Footer;
